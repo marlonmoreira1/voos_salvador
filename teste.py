7a2c2e6bcbde3e4c1866dfdc00313003030e70b3
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-08-13 16:49:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -134,8 +134,8 @@ voos['Airline'] = voos['Airline'].str.replace(r'\-$', '', regex=True)
 
 voos[['Aircraft', 'Aircraft_type']] = voos['Aircraft'].str.extract(r'(.+)\((.+)\)')
 
-voos[['Status', 'Hora_realizada','AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z]+)(\d{1,2}:\d{2})?(AM|PM)')
-print(voos['Hora_Realizada'])
+voos[['Status', 'Hora_realizada','AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z]+)(\d{1,2}:\d{2})\s?(AM|PM)')
+print(voos[['Hora_realizada','AM-PM_Realizado']])
 voos[['Time', 'AM-PM_Previsto']] = voos['Time'].str.extract(r'(\d{1,2}:\d{2})\s?(AM|PM)')
 
 url = "https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.75.zip"
