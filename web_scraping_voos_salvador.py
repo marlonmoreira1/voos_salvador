3d628eeb54d2b75bdc0bb7b810a244e9c01625d5
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2025-03-05 14:17:34 -0300

    Update web_scraping_voos_salvador.py

--- web_scraping_voos_salvador.py
+++ web_scraping_voos_salvador.py
@@ -21,7 +21,7 @@ import warnings
 warnings.simplefilter(action='ignore')
 
 options = Options()
-#options.add_argument('--headless')  
+options.add_argument('--headless')  
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
